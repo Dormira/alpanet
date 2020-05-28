b1c58c67e3e67c885f848bb491d2ebf7253c0b95
Angelica <angelica.heeney@gmail.com>
2020-05-27 22:57:23 -0700

    janky click and drag

---
+++ Assets/clickAndDrag.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class clickAndDrag : MonoBehaviour
+{
+    Vector3 screenPoint;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Offset is the difference between where I clicked and where the object is in world points
+    //Movement should act like I clicked the object's location
+    //We should take the difference between where we clicked and dragged in world point units
+    //Called when you click?
+
+    /*
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));//The original place that I clicked in world points
+        offset = this.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));//The difference between the object and where I clicked in world points
+        
+    }
+
+    void OnMouseDrag()
+    {
+        //This is clearly wrong.
+        //What we want is to get the object's current position
+        Vector3 curWorldPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));//The current place that my mouse is in world points
+        Vector3 diff = curWorldPoint - screenPoint;//The difference in how I moved my mouse in world points and where my mouse started in world points
+        
+      //  Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+        UnityEngine.Debug.Log("OFFSET IS " + offset);
+        UnityEngine.Debug.Log("DIFF IS " + diff);
+        UnityEngine.Debug.Log("MOVING OBJECT FROM "+transform.position+" TO "+(transform.position+diff+offset));
+     //   this.transform.position += offset+Camera.main.ScreenToWorldPoint(diff);
+    }
+
+    */
+
+    void OnMouseUp()
+    {
+        this.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    void OnMouseDown()
+    {
+        this.GetComponent<Rigidbody>().useGravity = false;
+        screenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);//The original place that I clicked in screen points
+        offset = this.transform.position - Camera.main.ScreenToWorldPoint(screenPoint);//The difference between the object and where I clicked in world points
+    }
+
+    void OnMouseDrag()
+    {
+        //This is clearly wrong.
+        //What we want is to get the object's current position
+        Vector3 curWorldPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);//The current place that my mouse is in screen points
+        Vector3 diff = curWorldPoint - screenPoint;//The difference in where I moved my mouse and where my mouse started, in screen points
+
+        //  Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+        if (diff != new Vector3(0, 0, 0))
+        {
+            UnityEngine.Debug.Log("OFFSET IS " + offset);
+            UnityEngine.Debug.Log("DIFF IS " + diff);
+            UnityEngine.Debug.Log("MOVING OBJECT FROM " + this.transform.position + " TO " + (this.transform.position + diff));
+            screenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+            offset = this.transform.position - Camera.main.ScreenToWorldPoint(screenPoint);
+            this.transform.position += diff;
+        }
+
+    }
+}
--- Assets/wiggle.cs
+++ Assets/wiggle.cs
@@ -23,18 +23,20 @@ public class wiggle : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        //Change the model vertices
-        Vector3[] nv = newVertices();
-        Vector2[] nuv = model.uv;//newUV(nv);
-        int[] nt = model.triangles;
+        if (Input.GetKeyDown("w"))
+        {
+            //Change the model vertices
+            Vector3[] nv = newVertices();
+            Vector2[] nuv = model.uv;//newUV(nv);
+            int[] nt = model.triangles;
 
-        //    model.Clear();
+            //    model.Clear();
 
-        model.vertices = nv;
-        model.uv = nuv;
-        model.triangles = nt;
-        MeshUtility.Optimize(model);
-
+            model.vertices = nv;
+            model.uv = nuv;
+            model.triangles = nt;
+            MeshUtility.Optimize(model);
+        }
         //        moveToTargetLocation();
     }
 
@@ -62,7 +64,7 @@ public class wiggle : MonoBehaviour {
     }
     Vector2[] newUV(Vector3[] vertices)
     {
-        Vector2[] uvs = new Vector2[vertices.Count()];
+        Vector2[] uvs = new Vector2[vertices.Length];
         for (int i = 0; i < uvs.Length; i++)
         {
             uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
