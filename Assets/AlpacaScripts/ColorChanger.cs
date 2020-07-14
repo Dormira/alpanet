726d6eb463e2aff18ce71d76f15d18aad88984ea
Angelica <angelica.heeney@gmail.com>
2020-07-13 17:38:41 -0700

    Many small updates
    Main menu start game script added
    Pause button start function culled
    Removed object location display GUI element
    Removed unnecessary asset
    Color change script update

---
+++ Assets/AlpacaScripts/ColorChanger.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ColorChanger : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            changeColors();
+        }
+    }
+
+    public void changeColors()
+    {
+        Mesh oldmesh = null;
+        //Figure out a better way than this thing yikes
+        if (GetComponent<MeshFilter>())
+        {
+            oldmesh = GetComponent<MeshFilter>().sharedMesh;
+        }
+        else if (GetComponent<SkinnedMeshRenderer>())
+        {
+            oldmesh = GetComponent<SkinnedMeshRenderer>().sharedMesh;
+        }
+
+        Mesh newmesh = (Mesh)Instantiate(oldmesh);
+
+        Color colorStart = new Color(UnityEngine.Random.Range(0f, 1f),
+            UnityEngine.Random.Range(0f, 1f),
+            UnityEngine.Random.Range(0f, 1f),
+            1f);
+        Color colorEnd = new Color(UnityEngine.Random.Range(0f, 1f),
+            UnityEngine.Random.Range(0f, 1f),
+            UnityEngine.Random.Range(0f, 1f),
+           1f);
+
+        Vector3[] vertices = oldmesh.vertices;
+        Color[] colors = new Color[vertices.Length];
+
+        for (int i = 0; i < vertices.Length; i++)
+            colors[i] = Color.Lerp(colorStart, colorEnd, vertices[i].y);
+
+
+        newmesh.vertices = vertices;
+        newmesh.colors = colors;
+
+
+        if (GetComponent<MeshFilter>())
+        {
+            GetComponent<MeshFilter>().sharedMesh = newmesh;
+        }
+        else if (GetComponent<SkinnedMeshRenderer>())
+        {
+            GetComponent<SkinnedMeshRenderer>().sharedMesh = newmesh;
+        }
+
+        //DestroyImmediate(oldmesh, true);
+
+        //Remove old mesh
+    }
+}
---
+++ Assets/UIScripts/menuStartGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuStartGame : MonoBehaviour
+{
+    UnityEngine.AsyncOperation loadGameOperation;
+    void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect((Screen.width/2)-100, (Screen.height/2)-25, 200, 50), "PRESS THIS PUTTON"))
+        {
+            loadGameOperation.allowSceneActivation = true;
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+
+        loadGameOperation = SceneManager.LoadSceneAsync("game");
+        loadGameOperation.allowSceneActivation = false;
+    }
+}
