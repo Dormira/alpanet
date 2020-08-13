2bd4f582c48fdb770e1029f4cc61e705930d26b2
Angelica <angelica.heeney@gmail.com>
2020-08-12 23:51:39 -0700

    Update generateTerrain.cs
    Cleaned up generateTerrain a little bit, changed some functions to be pass by reference

--- Assets/TerrainScripts/generateTerrain.cs
+++ Assets/TerrainScripts/generateTerrain.cs
@@ -8,80 +8,59 @@ public class generateTerrain : MonoBehaviour
 
     void Start()
     {
-        Terrain oldTerrain = this.GetComponent<Terrain>();
-        int terrainResolution = oldTerrain.terrainData.heightmapResolution;
+        Terrain terrain = this.GetComponent<Terrain>();
+        TerrainData terrainData = terrain.terrainData;
+        int terrainResolution = terrainData.heightmapResolution;
 
-
-        float[,] newHeightmap = generateHeightmap(terrainResolution);
-        oldTerrain.terrainData.SetHeights(0, 0, newHeightmap);
-        oldTerrain.terrainData.SyncHeightmap();
+        terrainData.SetHeights(0, 0, generateHeightmap(terrainResolution));
+        terrainData.SyncHeightmap();
 
 
         //TEXTURES
         int numTextures = 2;
         //Now do alphamap stuff at it
-        float[,,] alphamap = new float[oldTerrain.terrainData.alphamapWidth,
-                                       oldTerrain.terrainData.alphamapHeight, numTextures];
+        float[,,] alphamap = new float[terrainData.alphamapWidth,
+                                       terrainData.alphamapHeight, numTextures];
 
         for (int i = 0; i < terrainResolution - 1; i++)
         {
             for (int j = 0; j < terrainResolution - 1; j++)
             {
-                 alphamap[i, j, 0] = newHeightmap[i, j];
-                 alphamap[i, j, 1] = 1-newHeightmap[i, j];
-                //alphamap[i, j, 0] = 1;
-                //alphamap[i, j, 1] = 1;
-                //alphamap[i, j, 0] = 1;
+                alphamap[i, j, 0] = terrainData.GetHeight(j, i)/terrainData.alphamapHeight;
+                alphamap[i, j, 1] = 1-terrainData.GetHeight(j, i) / terrainData.alphamapHeight;
             }
         }
         //Set the alpha maps
-        oldTerrain.terrainData.SetAlphamaps(0, 0, alphamap);
+        terrainData.SetAlphamaps(0, 0, alphamap);
 
 
         //Generate grass texture -- THIS MUST HAPPEN AFTER HEIGHTS ARE SET IN ORDER TO CALCULATE STEEPNESS
-        int[,] grassLayer = oldTerrain.terrainData.GetDetailLayer(0, 0, oldTerrain.terrainData.detailHeight, oldTerrain.terrainData.detailWidth, 0);
-        oldTerrain.terrainData.SetDetailLayer(0, 0, 0, generateGrass(grassLayer, oldTerrain.terrainData));
-        /*
-        Texture2D tex = oldTerrain.terrainData.GetAlphamapTexture(0);
-        Color[] colors = new Color[tex.width*tex.height];
-        Color green = new Color(0, 1, 0, 1);
-        for(int i = 0; i < tex.width*tex.height; i++)
-        {
-            colors[i] = green;
-        }
-        tex.SetPixels(colors);
-        tex.Apply();
-
-        TerrainLayer[] terrainLayers = oldTerrain.terrainData.terrainLayers;
-        UnityEngine.Debug.Log("***");
-        UnityEngine.Debug.Log(terrainLayers[0].diffuseTexture);
-        UnityEngine.Debug.Log("***");
-        //terrainLayers[0].diffuseTexture = tex;
-        //terrainLayers[1].diffuseTexture = tex;
-        */
-
-
+        int[,] grassLayer = terrainData.GetDetailLayer(0, 0,terrainData.detailHeight, terrainData.detailWidth, 0);
+        generateGrass(ref grassLayer, ref terrainData);
+        terrainData.SetDetailLayer(0, 0, 0, grassLayer);
     }
 
     float[,] generateHeightmap(int terrainResolution)
     {
         System.Random random = new System.Random();
-
+        //maybe we'd be better served by setting the old one to zero
         float[,] newHeightmap = new float[terrainResolution, terrainResolution];
 
 
         int maxWidth = terrainResolution / 4;
+        int xWidth, yWidth, xStart, yStart, stripDirection;
+        float raiseHeight;
         for (int i = 0; i < 10; i++)
         {
-            int xWidth = random.Next(0, maxWidth);
-            int xStart = random.Next(0, terrainResolution - xWidth + 1);
-            int stripDirection = random.Next(0, 2);
-            float raiseHeight = random.Next(1, 4);
+            xWidth = random.Next(0, maxWidth);
+            xStart = random.Next(0, terrainResolution - xWidth + 1);
+            stripDirection = random.Next(0, 2);
+            raiseHeight = random.Next(1, 4);
 
             for (int j = xStart; j < xStart + xWidth; j++)
             {
-                int yWidth = random.Next(0, maxWidth);
-                int yStart = random.Next(0, terrainResolution - yWidth + 1);
+                yWidth = random.Next(0, maxWidth);
+                yStart = random.Next(0, terrainResolution - yWidth + 1);
                 for (int k = yStart; k < yStart+yWidth; k++)
                 {
                     if (stripDirection == 0)
@@ -96,24 +75,28 @@ public class generateTerrain : MonoBehaviour
             }
         }
 
-        newHeightmap = boxblur(newHeightmap);
-        newHeightmap = boxblur(newHeightmap);
+        boxblur(ref newHeightmap);
+        boxblur(ref newHeightmap);
         return newHeightmap;
         
         //Set the heights
 
     }
 
-    int[,] generateGrass(int[,] grassLayerToGenerate, TerrainData terrainData)
+    void generateGrass(ref int[,] grassLayerToGenerate, ref TerrainData terrainData)
     {
+        //Do the grass layer modifications in place to save memory
+        float normi;
+        float normj;
+        float steepness;
         for (int i = 0; i < terrainData.detailHeight; i++)
         {
             for (int j = 0; j < terrainData.detailWidth; j++)
             {
-                float normi = (float)((i / 2) * 1.0 / (terrainData.alphamapHeight - 1));
-                float normj = (float)((j / 2) * 1.0 / (terrainData.alphamapWidth - 1));
+                normi = (float)((i / 2) * 1.0 / (terrainData.alphamapHeight - 1));
+                normj = (float)((j / 2) * 1.0 / (terrainData.alphamapWidth - 1));
 
-                float steepness = Math.Abs(terrainData.GetSteepness(normi, normj));
+                steepness = Math.Abs(terrainData.GetSteepness(normi, normj));
 
                 if (steepness > 50)
                 {
@@ -126,22 +109,22 @@ public class generateTerrain : MonoBehaviour
 
             }
         }
-        return grassLayerToGenerate;
     }
 
-    float[,] boxblur(float[,] array)
+    void boxblur(ref float[,] array)
     {
-        float[,] newArray = array;
-
+        //Not exactly a boxblur anymore because we're not doing our blur on a fresh copy of array
+        //But it still smooths the contours of our heightmap without making webgl shit itself
         int dist = 16;
-        int length = newArray.GetLength(0);
-
+        int length = array.GetLength(0);
+        float sumval;
+        int num;
         for (int i = 0; i < length; i++)
         {
             for (int j = 0; j < length; j++)
             {
-                float sumval = 0;
-                int num = 0;
+                sumval = 0;
+                num = 0;
                 for (int m = -dist; m < dist; m++)
                 {
                     for (int n = -dist; n < dist; n++)
@@ -153,11 +136,9 @@ public class generateTerrain : MonoBehaviour
                         }
                     }
                 }
-                newArray[i, j] = sumval / num;
+                array[i, j] = sumval / num;
             }
         }
-
-        return newArray; 
     }
     
 }
